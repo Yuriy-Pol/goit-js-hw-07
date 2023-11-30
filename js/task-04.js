4eb3fbe91900964789ef5a21a2a8498ae58a5349
Yuriy-Pol <polozhevetsyura@gmail.com>
2023-11-30 17:49:39 +0200

    js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  const trimmedEmail = emailInput.value.trim();
+  const trimmedPassword = passwordInput.value.trim();
+
+  if (trimmedEmail === "" || trimmedPassword === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: trimmedEmail,
+    password: trimmedPassword,
+  };
+
+  console.log(formData);
+
+  this.reset();
+});
--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById("controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createButton.addEventListener("click", () => createBoxes(Number(input.value)));
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  if (amount < 1 || amount > 100) {
+    alert("Please enter a number between 1 and 100.");
+    return;
+  }
+
+  // Очистка попередніх елементів
+  boxesContainer.innerHTML = "";
+
+  const boxes = Array.from({ length: amount }, (_, index) => {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${30 + index * 10}px`;
+    box.style.height = `${30 + index * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    return box;
+  });
+
+  boxesContainer.append(...boxes);
+  input.value = "";
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
